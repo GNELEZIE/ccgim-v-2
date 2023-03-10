d9851e29c2c409b456911e02931b54608560176e
Gnelezie <zie.nanien@gmail.com>
2023-03-10 15:30:40 +0000

    Mise

--- function/domaine.php
+++ function/domaine.php
@@ -5,3 +5,10 @@ $cdn_domaine = 'http://localhost:8080/www/ccgim-v-2/cdn';
 $app_domaine = 'https://app.cabinet-ccgim.com';
 $app_cdn = 'https://app.cabinet-ccgim.com/cdn';
 $cookies_domaine = '.localhost:8080';
+
+/*$domaine = 'https://cabinet-ccgim.com';
+$cdn_domaine = 'https://cabinet-ccgim.com/cdn';
+
+$app_domaine = 'https://app.cabinet-ccgim.com';
+$app_cdn = 'https://app.cabinet-ccgim.com/cdn';
+$cookies_domaine = '.cabinet-ccgim.com';*/
